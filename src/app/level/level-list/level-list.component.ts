8c85ffb1df6626dacb81da1ee9388b46381a258b
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-07-10 00:12:38 +0200

    add real exercises: e001, e002, e101 and correct view app

--- src/app/level/level-list/level-list.component.ts
+++ src/app/level/level-list/level-list.component.ts
@@ -6,7 +6,7 @@ import {ProgressLevel} from "../../progress/models/progress-level";
 import {ProgressExercise} from "../../progress/models/progress-exercise";
 import {ActivatedRoute} from "@angular/router";
 import {AuthenticationService} from "../../auth/authentication.service";
-import {Exercise} from "../../user/models/exercise";
+
 
 @Component({
   selector: 'app-level-list',
@@ -22,7 +22,16 @@ export class LevelListComponent implements OnInit {
   user_id: string = "";
   progressBar: number[] = [];
   index: number = +this.route.snapshot.params['id']
-
+  levelDescription: string[] = [
+    "Aspiring amateur players are introduced to the practice system, acquire the basics of snooker and reach their initial practice targets.",
+    "Amateurs play snooker as a sport and consolidate their basic  skills.",
+    "The snooker player continuously trains on the basic of the practice system, strengthens his/her technique and reproduces successful shot.",
+    "The trained player is now able to apply the practice elements in a frame, using the required techniques.",
+    "Players can use the skills acquired during practice in competitive environments (tournaments).",
+    "Players are increasingly confident and secure in their game. Initial success at tournaments or championships.",
+    "Players aim at playing in the upper leagues and reaching the semi-finals of national tournaments.",
+    "Participation in national championships and the premier league is open to the player.",
+  ]
 
   constructor(private levelService: LevelService,
               private progressService: ProgressService,
@@ -34,6 +43,7 @@ export class LevelListComponent implements OnInit {
   ngOnInit(): void {
     this.user_id = this.getCurrentUserId();
     this.loadLevelProgress();
+
   }
 
 
