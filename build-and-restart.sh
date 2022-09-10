04b933173979e3f5ca7a5cf9bda5441e9122e74d
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-09-10 21:12:46 +0200

    docker 004

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t edusnookerfront .
 docker stop edusnookerfront
 docker rm edusnookerfront
-docker run -d -p 80:80 --name edusnookerfront edusnookerfront
+docker run -d -p 4200:4200 --name edusnookerfront edusnookerfront
