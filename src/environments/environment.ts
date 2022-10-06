7d85a1434a3d2bb997db460e903fe9a53e9c7a78
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-10-06 11:25:15 +0200

    stripe part3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  stripe: "Your_publishable_key-test"
+  stripe: "pk_test_51LpHi7LJcGDwiGcWEb7Lv2DYHryv3OkNsdQbubCvj0uRf9zeOLSxENDkEpqJGNIO7gsKQGYq27jdR2c3klXyMj3c00ivC4AuXe"
 };
 
 /*
