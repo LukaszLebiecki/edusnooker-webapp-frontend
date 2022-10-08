ea479a46f35685a06e78ee0c4cb1f98f202c10c8
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2022-10-08 14:15:21 +0200

    stripe part11

--- src/app/payments/success/success.component.ts
+++ src/app/payments/success/success.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NotificationType} from "../../notification/notification-type.enum";
+import {AuthenticationService} from "../../auth/authentication.service";
+import {Router} from "@angular/router";
+import {NotificationService} from "../../notification/notification.service";
+
 
 @Component({
   selector: 'app-success',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private authenticationService: AuthenticationService,
+              private notificationService: NotificationService,
+              private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  public logout() {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+    this.sendNotification(NotificationType.SUCCESS, `You've been successfully logged out`);
+  }
+
+  private sendNotification(notificationType: NotificationType, message: any): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occurred, Please try again.');
+    }
   }
 
 }
