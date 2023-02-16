8529336be57d97cd674b1fdb0b5690ca1ae6ceb2
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2023-02-16 22:21:01 +0100

    deployd part4

--- src/app/payments/payments/payments.component.ts
+++ src/app/payments/payments/payments.component.ts
@@ -17,7 +17,7 @@ export class PaymentsComponent implements OnInit {
   user: User
 
   purchaseStarted = false;
-  monthlyPriceId = "price_1Mb7eLLJcGDwiGcWljBHFsWC";
+  monthlyPriceId = "prod_NLpI2ukbIjTQCo";
   private subUrl: string = environment.subUrl;
   private apiUrl: string = environment.apiUrl;
 
